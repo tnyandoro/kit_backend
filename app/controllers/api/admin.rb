3d413a0a6922d64fbfc912c1f1da430f04a32afc
Tendai <tendai1@hotmail.com>
2026-06-13 22:35:11 +0200

    Add admin controller

---
+++ app/controllers/api/admin.rb
@@ -0,0 +1,21 @@
+# app/controllers/api/admin/base_controller.rb
+module Api
+  module Admin
+    class BaseController < ApplicationController
+      before_action :authenticate_admin!
+
+      private
+
+      def authenticate_admin!
+        token = request.headers['Authorization']&.split(' ')&.last
+        
+        begin
+          decoded = JsonWebToken.decode(token)
+          @current_admin = AdminUser.find(decoded[:admin_id])
+        rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+          render json: { error: "Unauthorized. Invalid or missing token." }, status: :unauthorized
+        end
+      end
+    end
+  end
+end
