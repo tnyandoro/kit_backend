722380699c143a0d83aecf0f9064dc778897c675
Tendai <tendai1@hotmail.com>
2026-06-14 18:57:33 +0200

    Set up an admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+AdminUser.create!(
+  email: "admin@kit.com",
+  password: "ChangeMe123!",
+  password_confirmation: "ChangeMe123!"
+)
