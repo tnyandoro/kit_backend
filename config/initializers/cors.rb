faf1cabbc7b3697b3d3322bedda58ef0a765f1e3
Tendai <tendai1@hotmail.com>
2026-06-14 16:53:22 +0200

    fix: update CORS to allow deployed frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # IMPORTANT: Add 8080 because that is where your React app is running!
-    origins "http://localhost:8080", "http://localhost:5173", "http://localhost:3000"
+    origins "https://kit.ac.zw","https://dulcet-travesseiro-ee79ad.netlify.app", "http://localhost:8080", "http://localhost:5173", "http://localhost:3000"
 
     resource "*",
       headers: :any,
