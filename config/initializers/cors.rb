b16338ba94e1b9fde09c5d76934e2afe2fcfc5ae
Tendai <tendai1@hotmail.com>
2026-06-17 17:48:31 +0200

    Fix all cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(
-    "http://localhost:8080",
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://kit.ac.zw",
-    "https://www.kit.ac.zw"
-    "https://dulcet-travesseiro-ee79ad.netlify.app"
-  )
+      "http://localhost:8080",
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://kit.ac.zw",
+      "https://www.kit.ac.zw",
+      "https://dulcet-travesseiro-ee79ad.netlify.app"
+    )
 
     resource "*",
       headers: :any,
